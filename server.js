d45f664a5a10bca3cbe1b2df9100b9fa56a7bb50
Albina93 <alba.tleubergen@gmail.com>
2026-05-21 00:12:36 -0400

    Add MongoDB connection and Express server setup

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json);
+const connectDB = require("./db/connection");
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
