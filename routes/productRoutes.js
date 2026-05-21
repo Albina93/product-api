5a9ca9ad0951b41474f063abe19fdfe055e939be
Albina93 <alba.tleubergen@gmail.com>
2026-05-21 01:32:46 -0400

    Add advanced GET /api/products with filtering, sorting and pagination

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -51,13 +51,53 @@ router.delete("/:id", async (req, res) => {
     if (!deletedProduct) {
       res.status(404).json({ error: "Product not found" });
     } else {
-      res
-        .status(200)
-        .json({
-          message: "Product deleted successfully",
-          product: deletedProduct,
-        });
+      res.status(200).json({
+        message: "Product deleted successfully",
+        product: deletedProduct,
+      });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Read all products with advanced querying
+router.get("/", async (req, res) => {
+  try {
+    // filter
+    const filter = {};
+    if (req.query.category) {
+      filter.category = req.query.category;
+    }
+    if (req.query.minPrice || req.query.maxPrice) {
+      filter.price = {};
+      if (req.query.minPrice) {
+        filter.price.$gte = Number(req.query.minPrice);
+      }
+      if (req.query.maxPrice) {
+        filter.price.$lte = Number(req.query.maxPrice);
+      }
+    }
+
+    // sort
+    const sort = {};
+
+    if (req.query.sortBy === "price_asc") {
+      sort.price = 1;
+    } else if (req.query.sortBy === "price_desc") {
+      sort.price = -1;
+    }
+
+    // pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find(filter)
+      .sort(sort)
+      .skip(skip)
+      .limit(limit);
+    res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
