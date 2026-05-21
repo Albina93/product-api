58301ae902ec347e644d8e5ee990afb7a31a4a24
Albina93 <alba.tleubergen@gmail.com>
2026-05-21 00:53:23 -0400

    Add GET and PUT /api/products/:id endpoints

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,4 +12,36 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Read single product
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      res.status(404).json({ error: "Product not found" });
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update product
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true },
+    );
+    if (!updatedProduct) {
+      res.status(404).json({ error: "Product not found" });
+    } else {
+      res.status(200).json(updatedProduct);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
